f3f1515f037333060f998255fa8093b5722dc888
Zac-HD <zac.hatfield.dodds@gmail.com>
2016-02-21 03:10:06 +1100

    Upgrade pointcloudfile substantially
    Much more flexible reader; should cope with any vertex attributes
    (assuming x, y, z are included).
    
    Reader is also more robust in both coping with valid-but-unexpected
    data, and exiting when it hits something it really can't cope with.
    
    Better typing data, misc tweaks, etc.  I suspect the _replace method of
    namedtuple is faster than unpacking, too.

--- src/pointcloudfile.py
+++ src/pointcloudfile.py
@@ -14,31 +14,36 @@ spatial coordinates.  RGB is the color, each an unsigned 8-bit integer.
 While intentionally limited in scope, most data can be converted to this
 format easily enough.
 """
-# pylint:disable=unsubscriptable-object
+# Pylint can freak out about mypy type notation; it's fine at runtime
+# pylint:disable=unsubscriptable-object,invalid-sequence-index
 
+from collections import namedtuple
+import itertools
 import struct
 import os.path
 from tempfile import SpooledTemporaryFile
-from typing import Iterator, Tuple
+from typing import Any, Iterator, List, Tuple
 
 from . import utm_convert
 
 # User-defined types:
-PointStream = Iterator[Tuple[float, ...]]
+Complicated = Tuple[int, Any, struct.Struct, List[str]]
 
 
 def offset_for(filename: str) -> Tuple[float, float, float]:
-    """Return the (x, y, z) offset for a Pix4D .ply cloud."""
+    """Return the (x, y, z) UTM offset for a Pix4D or forestutils .ply file."""
     offset = filename[:-4] + '_ply_offset.xyz'
-    try:
+    if os.path.isfile(offset):
         with open(offset) as f:
             x, y, z = tuple(float(n) for n in f.readline().strip().split(' '))
             return x, y, z
-    except FileNotFoundError:
-        return 0, 0, 0
+    for com in parse_ply_header(ply_header_text(filename))[3]:
+        if com.startswith('comment UTM x y zone south'):
+            return float(com.split(' ')[-4]), float(com.split(' ')[-3]), 0
+    return 0, 0, 0
 
 
-def _check_input(fname, ending=''):
+def _check_input(fname, ending='.ply'):
     """Checks that the file exists and has the right ending"""
     if not os.path.isfile(fname):
         raise FileNotFoundError('Cannot read points from a nonexistent file')
@@ -47,7 +52,7 @@ def _check_input(fname, ending=''):
             fname[-4:], ending))
 
 
-def read(fname: str) -> PointStream:
+def read(fname: str) -> Iterator:
     """Passes the file to a read function for that format."""
     if fname.endswith('_point_cloud_part_1.ply'):
         parts, p = [fname], 1
@@ -62,7 +67,7 @@ def read(fname: str) -> PointStream:
     return _read_ply(fname)
 
 
-def _read_pix4d_ply_parts(fname_list):
+def _read_pix4d_ply_parts(fname_list: List[str]) -> Iterator:
     """Yield points from a list of Pix4D ply files as if they were one file.
 
     Pix4D usually exports point clouds in parts, with an xyz offset for the
@@ -75,87 +80,87 @@ def _read_pix4d_ply_parts(fname_list):
     be noticible if we did.
     """
     for f in fname_list:
-        _check_input(f, '.ply')
+        _check_input(f)
     f = fname_list.pop(0)
     ox, oy, oz = offset_for(f)
-    for point in _read_ply(f):
-        x, y, z, r, g, b = point
-        yield x, y, z+oz, r, g, b
+    for p in _read_ply(f):
+        yield p._replace(z=p.z+oz)
     for f in fname_list:
         dx, dy, dz = [b - a for a, b in zip([ox, oy, 0], offset_for(f))]
-        for x, y, z, r, g, b in _read_ply(f):
-            yield x+dx, y+dy, z+dz, r, g, b
+        for p in _read_ply(f):
+            yield p._replace(x=p.x+dx)._replace(y=p.y+dy)._replace(z=p.z+dz)
 
 
-def process_header(file_handle):
-    """Return key information from the header of an open ``.ply`` file.
+def ply_header_text(filename: str) -> bytes:
+    """Return the exact text of the header of the given .ply file, as bytes.
 
-    Args:
-        file_handle: must be in binary read mode (``'rb'``), at start of file.
-
-    Returns:
-        (struct.Struct, indicies, offset): a tuple of ``struct`` instance for
-        the binary format of the file;  the respective indicies of XYZRGB
-        values within each element; and the UTM location of the pointcloud
-        origin coordinate (None if unknown).
+    Using bytes to allow len(header) to give index to start of data; it's
+    trivial to decode in the parsing function.
     """
-    # TODO:  detect unhandled file types for robustness
-    # TODO:  generalise for other attributes of pointclouds
-    # TODO:  return number of verticies, to avoid reading other elements
-    head = []
-    while ['end_header'] not in head:
-        head.append(next(file_handle).decode().strip().split(' '))
-
-    is_big_endian = ['format', 'binary_big_endian', '1.0'] in head
-    while head[-1] != ['end_header']:
-        head.pop()
-        if not head:
-            raise ValueError('Invalid header for .ply file.')
-
-    offset = None
-    for line in head:
-        if line[:6] == ['comment', 'UTM', 'x', 'y', 'zone', 'south']:
-            x, y, zone, S = line[-4:]
-            offset = utm_convert.UTM_coords(
-                float(x), float(y), int(zone), bool(int(S)))
-
-    typeorder = [line[1] for line in head if line[0] == 'property']
-    form_str = '>' if is_big_endian else '<'
-    ply_types = {'float': 'f', 'double': 'd',
-                 'uchar': 'B', 'char': 'b',
-                 'ushort': 'H', 'short': 'h',
-                 'uint': 'I', 'int': 'i'}
-    form_str += ''.join(ply_types.get(t, '') for t in typeorder)
-
-    head = [line[2] for line in head if line[0] == 'property']
-    cols = {'r': 'red', 'g': 'green', 'b': 'blue'}
-    if any(l.startswith('diffuse_') for l in head):
-        cols = {k: 'diffuse_'+v for k, v in cols.items()}
-    ind = {c: head.index(c) for c in 'xyz'}
-    ind.update({c: head.index(cols[c]) for c in 'rgb'})
-    ind = tuple(ind[c] for c in 'xyzrgb')
-
-    return struct.Struct(form_str), ind, offset
+    _check_input(filename)
+    header = b''
+    with open(filename, 'rb') as f:
+        while b'end_header' not in header:
+            header += next(f)  # type: ignore
+    return header
 
 
-def _read_ply(fname) -> PointStream:
+def parse_ply_header(header_text: bytes) -> Complicated:
+    """Parse the bytes of a .ply header to useful data about the vertices.
+
+    Deliberately discards the non-vertex data - this is a pointcloud module!
+    """
+    # Get file lines, do some basic validation
+    lines = [l.strip() for l in header_text.decode('ascii').split('\n')]
+    magic_num, data_format, *lines = lines
+    if magic_num != 'ply':
+        raise ValueError('Not a valid .ply file (wrong magic number).')
+    if not data_format.startswith('format'):
+        raise ValueError(
+            'Unknown data format "{}" for .ply file.'.format(data_format))
+    if 'ascii' in data_format:
+        raise ValueError('ASCII format .ply files not supported at this time.')
+
+    # Extract comments from lines
+    comments = [c for c in lines if c.startswith('comment ')]
+    lines = [l for l in lines if not l.startswith('comment ')]
+
+    # Get vertex count
+    element, _, vertex_count = lines.pop(0).rpartition(' ')
+    if element != 'element vertex':
+        raise ValueError('File must begin with vertex data!')
+
+    # Get list of (type, name) pairs from the list of vertex properties
+    lines = [l.split(' ') for l in lines]  # type: ignore
+    properties = [(t, n) for _, t, n in
+                  itertools.takewhile(lambda l: l[0] == 'property', lines)]
+
+    # Get Struct format from list of property types
+    PLY_TYPES = {'float': 'f', 'double': 'd', 'uchar': 'B', 'char': 'b',
+                 'ushort': 'H', 'short': 'h', 'uint': 'I', 'int': 'i'}
+    form_str = '>' if 'binary_big_endian' in data_format else '<'
+    form_str += ''.join(PLY_TYPES[t] for t, n in properties)
+
+    # Get Namedtuple instance from property names
+    names = [n for t, n in properties]
+    if not all(p in names for p in ('x', 'y', 'z')):
+        raise ValueError('Pointcloud verticies must have x, y, z attributes!')
+    point_tuple = namedtuple('Point', names)  # type: ignore
+
+    # Finally, return our values
+    return int(vertex_count), point_tuple, struct.Struct(form_str), comments
+
+
+def _read_ply(fname: str) -> Iterator:
     """Opens the specified file, and returns a point set in the format required
     by attributes_from_cloud.  Only handles xyzrgb point clouds, but that's
     a fine subset of the format.  See http://paulbourke.net/dataformats/ply/"""
-    _check_input(fname, '.ply')
+    header = ply_header_text(fname)
+    vertex_count, Point, fmt, _ = parse_ply_header(header)
     with open(fname, 'rb') as f:
-        point, ind, _ = process_header(f)
-        raw = f.read(point.size)
-        if ind == tuple(range(6)) and point.size == 6:
-            # This special case is also most common, so we handle it faster
-            while raw:
-                yield point.unpack(raw)
-                raw = f.read(point.size)
-        else:
-            while raw:
-                p_tup = point.unpack(raw)
-                yield tuple(p_tup[i] for i in ind)
-                raw = f.read(point.size)
+        f.seek(len(header))
+        for _ in range(vertex_count):  # type: ignore
+            yield Point(*fmt.unpack(f.read(fmt.size)))
 
 
 class IncrementalWriter:
@@ -200,12 +205,12 @@ class IncrementalWriter:
                 in that order.  (x, y, z) values are coerced to 32-bit floats.
                 (red, green, blue) values are coerced to 8-bit unsigned ints.
         """
-        self.temp_storage.write(self.binary.pack(*point))
+        # TODO:  URGENT - make writer method flexible enough to match reader
+        self.temp_storage.write(self.binary.pack(*point[:6]))
         self.count += 1
 
-    def save_to_disk(self):
+    def __del__(self):
         """Flush data to disk and clean up."""
-        # TODO:  could this be a __del__ method??
         head = ['ply',
                 'format binary_little_endian 1.0',
                 'element vertex {}'.format(self.count),
@@ -237,4 +242,3 @@ def write(cloud, fname, utm_coords=None):
     writer = IncrementalWriter(fname, utm_coords)
     for p in cloud:
         writer(p)
-    writer.save_to_disk()
